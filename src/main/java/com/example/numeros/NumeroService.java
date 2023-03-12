bd51b119153079ec6c2d06d7103918563742207b
Brian Steven <sbrianprog@gmail.com>
2023-03-12 16:50:01 -0500

    commit

--- src/main/java/com/example/numeros/NumeroService.java
+++ src/main/java/com/example/numeros/NumeroService.java
@@ -1,5 +1,10 @@
 package com.example.numeros;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class NumeroService {
 
     public Integer obtenerTotal(Integer n) {
@@ -23,6 +28,26 @@ public class NumeroService {
          * numeros a descartar = 13, 130..139 , 1300...1399.
          * total numeros: 3699
          */
-        return n;
+
+        //Exercise as I understand
+        IntStream.range(1, n)
+                .filter(num -> !Integer.toString(num).contains("13"))
+                .forEach(num -> System.out.println(num));
+
+
+        long count = IntStream.range(1,n)
+                .filter(num -> !Integer.toString(num).contains("13"))
+                .count();
+        System.out.println("Count: " + count);
+
+        //Exercise as Mateo says
+        long count2 = IntStream.range(0,n)
+                .filter(num -> !Integer.toString(num).startsWith("13"))
+                .count();
+        System.out.println("Count: " + count2);
+
+        int numeros = (int) count2;
+        return numeros;
+
     }
 }
